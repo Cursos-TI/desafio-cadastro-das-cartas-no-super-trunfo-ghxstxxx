b6d068dbba8e2a3353403c33b8af8ccdf6d94bf7
ghxstxxx <nouveaujesse@gmail.com>
2025-02-17 13:55:41 +0000

    coloquei os dados do pais e a obs

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,19 +2,27 @@
 
 int main() {
    
-    char nome[20] = "Russia";
-    char populacao = "143000.00";
-    char pib = "13817.05";
+    char estado = "A";
+    char codigo [20] = "A01";
+    char cidade[20] = "Moscou";
+    int populacao = "12476200";
+    char area[20] = "2.511km2";
+    char pib[50] = "4.10 trilhões de dólares";
     int turismo = "20";
 
-    printf("Digite um pais: \n");
-    scanf("%s", &nome);
+    printf("Digite o estado: \n");
+    scanf("%c", &estado);
+    printf("Digite uma cidade: \n");
+    scanf("%s", &cidade);
     printf("Digite a populacao: \n");
-    scanf(" %d", &populacao);
+    scanf(" %f", &populacao);
     printf("Digite o PIB: \n");
-    scanf("%d", &pib);
+    scanf("%s", &pib);
     printf("Quantos pontos turisticos? \n");
-    scanf("%¨d", &turismo);
+    scanf("%d", &turismo);
+    printf("Quantos km2 tem Moscou? \n")
+    scanf("%f", &area);
+    printf("qual o codigo? \n");
+    scanf("%s", &codigo)
 
-    
 }
