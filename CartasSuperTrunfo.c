b04dfbc79a89bfbd3af2c178e2fe7298b86d57cd
ghxstxxx <nouveaujesse@gmail.com>
2025-02-20 17:54:25 +0000

    alteração no codigo e cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,28 +2,23 @@
 
 int main() {
    
-    char estado = "A";
+    char estado[20] = "São Paulo";
     char codigo [20] = "A01";
-    char cidade[20] = "Moscou";
-    int populacao = "12476200";
-    char area[20] = "2.511km2";
-    char pib[50] = "4.10 trilhões de dólares";
-    int turismo = "20";
+    char cidade[20] = "Guarulhos";
+    int populacao = "1.292.000";
+    char area = 319.200;
+    char pib = 77376467;
+    char turismo = 10;
 
-    // nao consigo entender qual o erro que esta acontecendo 
+    printf("Estado: %s\n", &estado);
+    printf("Código: %s\n", &codigo);
+    printf("Cidade: %s\n", &cidade);
+    printf("População: %f\n", &populacao);
+    printf("Area em KM2: %c\n", &area);
+    printf("PIB: %c\n", &pib);
+    printf("pontos turisticos: %c\n", &turismo);
     
-    printf("Digite o estado: \n");
-    scanf("%c", &estado);
-    printf("Digite uma cidade: \n");
-    scanf("%s", &cidade);
-    printf("Digite a populacao: \n");
-    scanf(" %f", &populacao);
-    printf("Digite o PIB: \n");
-    scanf("%s", &pib);
-    printf("Quantos pontos turisticos? \n");
-    scanf("%d", &turismo);
-    printf("Quantos km2 tem Moscou? \n");
-    scanf("%f", &area);
-    printf("qual o codigo? \n");
-    scanf("%s", &codigo);
+    
+   // os numeros aparecem como caracteres irreconheciveis ou zerados //
+    
 }
